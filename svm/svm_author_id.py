de548cf68fc0891dd68e365e887af7cfc336e8c8
Cloudtree11 <yunkun233@gmail.com>
2017-10-16 19:41:26 +0800

    Update mini project codes

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,6 +24,32 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+
+#clf = svm.SVC(kernel='linear')
+clf = svm.SVC(kernel='rbf', C=10000.0)
+print clf.kernel
+
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+
+count = 0
+for pred_t in pred:
+    if pred_t == 1:
+        count += 1
+print count
+
+#print pred[10], pred[26], pred[50]
+#accur = clf.score(features_test, labels_test)
+#print accur
 
 #########################################################
 
